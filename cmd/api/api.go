4a70412b40b27b822afa09bfa46d542cb9fa1eba
schorsche94 <gcasandra94@gmail.com>
2026-06-21 17:41:38 +0300

    - added docker-compose.yaml with postgres; - set up HTTP server;

---
+++ cmd/api/api.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+}
+type config struct {
+	addr string
+}
+
+func (app *application) mount() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+	}
+	log.Printf("server has started at %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module enrichment-service
+
+go 1.26
+
+require (
+	github.com/joho/godotenv v1.5.1
+)
